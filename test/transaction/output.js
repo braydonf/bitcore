a23b364d3d51722cb82e9e20b5d8be9a3e4a67a8
Braydon Fuller <braydon@bitpay.com>
2015-05-13 09:38:58 -0400

    Fixed issue with handling invalid output scripts.

--- test/transaction/output.js
+++ test/transaction/output.js
@@ -121,6 +121,34 @@ describe('Output', function() {
     expectEqualOutputs(newOutput, otherOutput);
   });
 
+  it('toObject will handle an invalid (null) script', function() {
+    // block 000000000000000b7e48f88e86ceee3e97b4df7c139f5411d14735c1b3c36791 (livenet)
+    // transaction index 2
+    // txid ebc9fa1196a59e192352d76c0f6e73167046b9d37b8302b6bb6968dfd279b767
+    var transaction = bitcore.Transaction();
+    transaction.fromString('01000000019ac03d5ae6a875d970128ef9086cef276a1919684a6988023cc7254691d97e6d010000006b4830450221009d41dc793ba24e65f571473d40b299b6459087cea1509f0d381740b1ac863cb6022039c425906fcaf51b2b84d8092569fb3213de43abaff2180e2a799d4fcb4dd0aa012102d5ede09a8ae667d0f855ef90325e27f6ce35bbe60a1e6e87af7f5b3c652140fdffffffff080100000000000000010101000000000000000202010100000000000000014c0100000000000000034c02010100000000000000014d0100000000000000044dffff010100000000000000014e0100000000000000064effffffff0100000000');
+    var obj = transaction.toObject();
+    obj.outputs[2].script.should.equal('4c');
+    obj.outputs[4].script.should.equal('4d');
+    obj.outputs[6].script.should.equal('4e');
+  });
+
+  it('#toObject roundtrip will handle an invalid (null) script', function() {
+    var invalidOutputScript = new Buffer('0100000000000000014c', 'hex');
+    var br = new bitcore.encoding.BufferReader(invalidOutputScript);
+    var output = Output.fromBufferReader(br);
+    var output2 = new Output(output.toObject());
+    should.equal(output2.script, null);
+    should.equal(output2._scriptBuffer.toString('hex'), '4c');
+  });
+
+  it('inspect will work with an invalid (null) script', function() {
+    var invalidOutputScript = new Buffer('0100000000000000014c', 'hex');
+    var br = new bitcore.encoding.BufferReader(invalidOutputScript);
+    var output = Output.fromBufferReader(br);
+    output.inspect().should.equal('<Output (1 sats) 4c>');
+  });
+
   it('roundtrips to/from JSON', function() {
     var json = output2.toJSON();
     var o3 = new Output(json);
